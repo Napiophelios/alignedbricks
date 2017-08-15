8db3b15129bd4f242d59ca9563a093bc1a635d3a
Napiophelios <napiophelios@gmail.com>
2017-08-15 08:59:14 -0400

    condensed code a bit simplified dslab recipe use alternate textures for default:brick node

--- init.lua
+++ init.lua
@@ -1,127 +1,104 @@
 --[[
-AlignedBricks mod for Minetest_game 0.4.16
-by Napiophelios
-
-Depends : default, stairs
-
-All code/textures derived from Minetest_game
-License of code: LGPL v2.1
-License of textures:
-Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+--
+-- AlignedBricks mod for Minetest_game 0.4.16
+-- by Napiophelios
+--
+-- Depends :
+-- default
+-- stairs
+-- mudbrick?
+-- nether?
+-- xdecor?
+--
+-- All code/textures derived from Minetest_game
+--
+-- License of code:
+-- LGPL v2.1
+--
+-- License of textures:
+-- Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+--
 --]]
 
 alignedbricks = {}
 
+-- override default bricks { "<node name>", "<image name>" }
+local bricks = {
+	{"brick", "brick"},
+	{"desert_sandstone_brick", "desert_sandstone_brick"},
+	{"desert_stonebrick", "desert_stone_brick"},
+	{"obsidianbrick", "obsidian_brick"},
+	{"sandstonebrick", "sandstone_brick"},
+	{"silver_sandstone_brick", "silver_sandstone_brick"},
+	{"stonebrick", "stone_brick"},
+}
 
-minetest.override_item("default:brick", {
-   tiles = {"alignedbricks_brick_top.png",
-  "alignedbricks_brick_top.png^[transformFXR90",
-  "alignedbricks_brick_side.png",
-  "alignedbricks_brick_side.png^[transformFX",
-  "alignedbricks_brick.png^[transformFX",
-  "alignedbricks_brick.png"},
-})
+for _, bricks in pairs(bricks) do
 
-minetest.override_item("default:desert_sandstone_brick", {
-  tiles = {"alignedbricks_desert_sandstone_brick_top.png",
-  "alignedbricks_desert_sandstone_brick_top.png^[transformFXR90",
-  "alignedbricks_desert_sandstone_brick_side.png",
-  "alignedbricks_desert_sandstone_brick_side.png^[transformFX",
-  "alignedbricks_desert_sandstone_brick.png^[transformFX",
-  "alignedbricks_desert_sandstone_brick.png"},
-})
-
-minetest.override_item("default:desert_stonebrick", {
-  tiles = {"alignedbricks_desert_stone_brick_top.png",
-  "alignedbricks_desert_stone_brick_top.png^[transformFXR90",
-  "alignedbricks_desert_stone_brick_side.png",
-  "alignedbricks_desert_stone_brick_side.png^[transformFX",
-  "alignedbricks_desert_stone_brick.png^[transformFX",
-  "alignedbricks_desert_stone_brick.png"},
-})
-
-minetest.override_item("default:obsidianbrick", {
-  tiles = {"alignedbricks_obsidian_brick_top.png",
-  "alignedbricks_obsidian_brick_top.png^[transformFXR90",
-  "alignedbricks_obsidian_brick_side.png",
-  "alignedbricks_obsidian_brick_side.png^[transformFX",
-  "alignedbricks_obsidian_brick.png^[transformFX",
-  "alignedbricks_obsidian_brick.png"},
-})
-
-minetest.override_item("default:sandstonebrick", {
-  tiles = {"alignedbricks_sandstone_brick_top.png",
-  "alignedbricks_sandstone_brick_top.png^[transformFXR90",
-  "alignedbricks_sandstone_brick_side.png",
-  "alignedbricks_sandstone_brick_side.png^[transformFX",
-  "alignedbricks_sandstone_brick.png^[transformFX",
-  "alignedbricks_sandstone_brick.png"},
-})
-
-minetest.override_item("default:silver_sandstone_brick", {
-  tiles = {"alignedbricks_silver_sandstone_brick_top.png",
-  "alignedbricks_silver_sandstone_brick_top.png^[transformFXR90",
-  "alignedbricks_silver_sandstone_brick_side.png",
-  "alignedbricks_silver_sandstone_brick_side.png^[transformFX",
-  "alignedbricks_silver_sandstone_brick.png^[transformFX",
-  "alignedbricks_silver_sandstone_brick.png"},
-})
-
-minetest.override_item("default:stonebrick", {
-  tiles = {"alignedbricks_stone_brick_top.png",
-  "alignedbricks_stone_brick_top.png^[transformFXR90",
-  "alignedbricks_stone_brick_side.png",
-  "alignedbricks_stone_brick_side.png^[transformFX",
-  "alignedbricks_stone_brick.png^[transformFX",
-  "alignedbricks_stone_brick.png"},
-})
+	minetest.override_item("default:" .. bricks[1] .. "", {
+		tiles = {
+		"alignedbricks_" .. bricks[2] .. "_top.png",
+		"alignedbricks_" .. bricks[2] .. "_top.png^[transformFXR90",
+		"alignedbricks_" .. bricks[2] .. "_side.png",
+		"alignedbricks_" .. bricks[2] .. "_side.png^[transformFX",
+		"alignedbricks_" .. bricks[2] .. ".png^[transformFX",
+		"alignedbricks_" .. bricks[2] .. ".png"},
+	})
+	
+end
 
 if minetest.get_modpath( 'mudbrick') then
+
 	minetest.override_item("mudbrick:mudbrick_1", {
-	   tiles = {"alignedbricks_mudbrick_wet_top.png",
-	  "alignedbricks_mudbrick_wet_top.png^[transformFXR90",
-	  "alignedbricks_mudbrick_wet_side.png",
-	  "alignedbricks_mudbrick_wet_side.png^[transformFX",
-	  "alignedbricks_mudbrick_wet.png^[transformFX",
-	  "alignedbricks_mudbrick_wet.png"},
+		tiles = {"alignedbricks_mudbrick_wet_top.png",
+		"alignedbricks_mudbrick_wet_top.png^[transformFXR90",
+		"alignedbricks_mudbrick_wet_side.png",
+		"alignedbricks_mudbrick_wet_side.png^[transformFX",
+		"alignedbricks_mudbrick_wet.png^[transformFX",
+		"alignedbricks_mudbrick_wet.png"},
 	})
 
 	minetest.override_item("mudbrick:mudbrick_2", {
-	   tiles = {"alignedbricks_mudbrick_dried_top.png",
-	  "alignedbricks_mudbrick_dried_top.png^[transformFXR90",
-	  "alignedbricks_mudbrick_dried_side.png",
-	  "alignedbricks_mudbrick_dried_side.png^[transformFX",
-	  "alignedbricks_mudbrick_dried.png^[transformFX",
-	  "alignedbricks_mudbrick_dried.png"},
+		tiles = {"alignedbricks_mudbrick_dried_top.png",
+		"alignedbricks_mudbrick_dried_top.png^[transformFXR90",
+		"alignedbricks_mudbrick_dried_side.png",
+		"alignedbricks_mudbrick_dried_side.png^[transformFX",
+		"alignedbricks_mudbrick_dried.png^[transformFX",
+		"alignedbricks_mudbrick_dried.png"},
 	})
+	
 end
 
 if minetest.get_modpath( 'nether') then
+
 	minetest.override_item("nether:brick", {
-	   tiles = {"alignedbricks_nether_brick_top.png",
-	  "alignedbricks_nether_brick_top.png^[transformFXR90",
-	  "alignedbricks_nether_brick_side.png",
-	  "alignedbricks_nether_brick_side.png^[transformFX",
-	  "alignedbricks_nether_brick.png^[transformFX",
-	  "alignedbricks_nether_brick.png"},
+		tiles = {"alignedbricks_nether_brick_top.png",
+		"alignedbricks_nether_brick_top.png^[transformFXR90",
+		"alignedbricks_nether_brick_side.png",
+		"alignedbricks_nether_brick_side.png^[transformFX",
+		"alignedbricks_nether_brick.png^[transformFX",
+		"alignedbricks_nether_brick.png"},
 	})
+	
 end
 
 if minetest.get_modpath( 'xdecor') then
+
 	minetest.override_item("xdecor:moonbrick", {
-	   tiles = {"alignedbricks_moonbrick_top.png",
-	  "alignedbricks_moonbrick_top.png^[transformFXR90",
-	  "alignedbricks_moonbrick_side.png",
-	  "alignedbricks_moonbrick_side.png^[transformFX",
-	  "alignedbricks_moonbrick.png^[transformFX",
-	  "alignedbricks_moonbrick.png"},
+		tiles = {"alignedbricks_moonbrick_top.png",
+		"alignedbricks_moonbrick_top.png^[transformFXR90",
+		"alignedbricks_moonbrick_side.png",
+		"alignedbricks_moonbrick_side.png^[transformFX",
+		"alignedbricks_moonbrick.png^[transformFX",
+		"alignedbricks_moonbrick.png"},
 	})
+	
 end
 
 
---stacked block slabs
+-- register stacked block slabs
 alignedbricks.register = function(dslabs_name, dslabs_desc, dslabs_images, dslabs_groups, dslabs_sounds, dslabs_craftitem)
--- inventory node
+
 	minetest.register_node(dslabs_name, {
 		description = dslabs_desc,
 		tiles = dslabs_images,
@@ -129,33 +106,27 @@ alignedbricks.register = function(dslabs_name, dslabs_desc, dslabs_images, dslab
 		groups = dslabs_groups,
 		sounds = dslabs_sounds,
 	})
+
 -- crafting recipe
 	minetest.register_craft({
-		output = dslabs_name .. ' 3',
+		output = dslabs_name .. ' 2',
 		recipe = {
-			{dslabs_craftitem, '', dslabs_craftitem},
 			{dslabs_craftitem, '', dslabs_craftitem},
 			{dslabs_craftitem, '', dslabs_craftitem},
 		},
 	})
--- alternate crafting recipe
-	minetest.register_craft({
-		output = dslabs_name .. ' 3',
-		recipe = {
-			{dslabs_craftitem, dslabs_craftitem, dslabs_craftitem},
-			{dslabs_craftitem, dslabs_craftitem, dslabs_craftitem},
-		},
-	})
---reverse recipe
+
+-- reverse recipe
 	minetest.register_craft({
 		output = dslabs_craftitem .. ' 2',
 		recipe = {
 			{dslabs_name},
 		},
 	})
+	
 end
 
-
+-- stacked block slabs
 alignedbricks.register(
 	"alignedbricks:dslabs_bronzeblock",
 	"Stacked Bronze Block Slabs",
